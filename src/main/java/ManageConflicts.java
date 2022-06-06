46f8cdec640770ae6b2eb6c3234d2406600c26f9
Lauren Velegol <lev14@scasd.org>
2022-06-06 11:06:08 -0400

    Add our class level JavaDoc

--- src/main/java/ManageConflicts.java
+++ src/main/java/ManageConflicts.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 
 //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
 
+
+/*This class manages conflicts */
 public class ManageConflicts {
     private HashMap<String, ArrayList<Integer>> usersAndConflicts = new HashMap();
 }
