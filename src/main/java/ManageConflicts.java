6200a6a52fb4efbcfb63fcee6a55527eb1f23382
Lauren Velegol <lev14@scasd.org>
2022-06-06 11:01:43 -0400

    Initial commit

---
+++ src/main/java/ManageConflicts.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+//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
+
+public class ManageConflicts {
+    private HashMap<String, ArrayList<Integer>> usersAndConflicts = new HashMap();
+}
